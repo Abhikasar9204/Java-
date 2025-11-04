04f91fdc0e7b0e571e3c8e877681e5687b0af3c4
Abhikasar9204 <abhinandankasar9204@gmail.com>
2025-11-04 11:40:22 +0530

    new change

---
+++ DecimalTobinary.java
@@ -0,0 +1,17 @@
+public class DecimalTobinary {
+    public static void dcitobin(int mynum){
+        int binnum=mynum;
+        int bin=0;
+        int pow=0;
+        while(binnum>0){
+            int rem=binnum%2;
+            bin=bin+rem*(int)Math.pow(2, pow);
+            binnum=binnum/2;
+            pow++;
+        }
+        System.out.println(bin);
+    }
+    public static void main(String args[]){
+        dcitobin(7);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Java
 This is the collections of code in the journey of learning Data Structure and Algorithm(DSA) in Java.
+it is the code.
